c787770b82d29d7dbb46b6ce6a924a586011e28f
neonaot <alisaneo652>
2021-10-07 14:47:37 +0300

    add parameters

--- scripts/with_pygithub/notifier.py
+++ scripts/with_pygithub/notifier.py
@@ -11,18 +11,19 @@ if __name__ == '__main__':
     parser.add_argument("--pr_id", type=int, required=True)
     parser.add_argument("--event", type=str, required=True)
     parser.add_argument("--label", type=str)
+    parser.add_argument("--rep", type=str)
     args = parser.parse_args()
 
     # TODO remove it
     print(args.label)
 
     g = Github(args.token)
-    repo = g.get_repo("neonaot/intellij-rust")
+    repo = g.get_repo("neonaot/intellij-rust") #TODO
 
-    if args.event == "merging":
+    if args.event == "closed":
         if DOC_LABEL in [i.name for i in repo.get_pull(args.pr_id).labels]:
             repo.get_issue(args.pr_id).create_comment(DOC_MSG)
-    elif args.event == "labeling": # and args.label == DOC_LABEL: #TODO
+    elif args.event == "labeled": # and args.label == DOC_LABEL: #TODO
 #         if repo.get_pull(args.pr_id).is_merged():
           repo.get_issue(args.pr_id).create_comment(DOC_MSG)
     else:
